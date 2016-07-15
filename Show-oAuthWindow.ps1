09aa1d109bfa3be515d40160b573a4b4fcbf0c96
Stephen Owen <Stephen@foxdeploy.com>
2016-07-15 12:37:03 -0400

    First push

---
+++ Show-oAuthWindow.ps1
@@ -0,0 +1,31 @@
+﻿   #region mini window, made by (Insert credits here)
+    Function Show-OAuthWindow {
+    param($url)
+    Add-Type -AssemblyName System.Windows.Forms
+ 
+    $form = New-Object -TypeName System.Windows.Forms.Form -Property @{Width=440;Height=640}
+    $web  = New-Object -TypeName System.Windows.Forms.WebBrowser -Property @{Width=420;Height=600;Url=($url -f ($Scope -join "%20")) }
+    $DocComp  = {
+            $Global:uri = $web.Url.AbsoluteUri
+            if ($Global:Uri -match "error=[^&]*|code=[^&]*") {$form.Close() }
+    }
+    $web.ScriptErrorsSuppressed = $true
+    $web.Add_DocumentCompleted($DocComp)
+    $form.Controls.Add($web)
+    $form.Add_Shown({$form.Activate()})
+    $form.ShowDialog() | Out-Null
+
+    $code = $web.DocumentText.Split("`n")[9].Trim()
+    $global:code = $code 
+    
+    if ($code.Length -eq 64){
+        $global:code = $code 
+        Write-host "we retrieved a code, check within `$code for the value"
+        }else
+        {
+        Write-Warning "The code we got back wasn't 32 chars in length, double check and try again"
+        }
+    #endregion
+
+    #login to get an access code 
+}
---
+++ trello login.ps1
@@ -0,0 +1,16 @@
+﻿$u = "stephen@foxdeploy.com"
+
+$trellokey ='0abd62664b472dd2895a372dbce0fbeb'
+
+$r = Show-OAuthWindow "https://trello.com/1/authorize?expiration=never&scope=read,write,account&response_type=token&name=Server%20Token&key=$trellokey"
+
+$t = '1edd9a7b0d4ea3deb697d1e53abdf5420d0f1a225184d4860f23c0fc3cfb16cc'
+$postParams = @{method='password';'factors[user]'=$u;'factors[password]'=$p}
+
+$r = Invoke-RestMethod -Uri https://trello.com/1/authentication -Method POST -Body $postParams -SessionVariable cookie
+
+$result = Invoke-WebRequest -uri https://trello.com/ -WebSession $cookie
+
+$me = invoke-restmethod "https://api.trello.com/1/members/Me/boards?key=$trellokey&token=$t"
+
+ "https://api.trello.com/1/members/Me?fields=username,fullName,url&boards=all&board_fields=name&organizations=all&organization_fields=displayName&key=$trellokey&token=$code"
